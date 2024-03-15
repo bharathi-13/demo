661f830931eb9b7f318bb64ef5662a8a6fac54e5
bharathi-13 <sathyabharathi00131@gmail.com>
2024-03-15 18:20:55 +0530

    removed some line

--- script.sh
+++ script.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# test line
-# status=$(systemctl is-active $service)
-
-# if [ $status == "inactive" ]
-# then
-#     echo "your service is not active"
-#     systemctl start $service
-#     echo "Done"
-# else
-#     echo "it is already active"
-# fi
 
 users=("user1" "user2" "user3")
 
